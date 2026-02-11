51998bf7919afa4f82c1df775f022f2aaf8ccf8e
Bharath <76623645+bharath-arch@users.noreply.github.com>
2026-02-11 15:34:58 +0530

    Update baseURL for production deployment

--- src/services/service.ts
+++ src/services/service.ts
@@ -3,8 +3,8 @@ import Cookies from "js-cookie";
 
 export const config = {
   //TODO: need to change this base url when deploying to production
-  baseURL: `http://localhost:4000/api/v1`,
-  // baseURL: "http://44.222.35.138/api/api/v1",
+  // baseURL: `http://localhost:4000/api/v1`,
+  baseURL: "http://44.222.35.138/api/api/v1",
   headers: {
     "Content-Type": "application/json",
   },
